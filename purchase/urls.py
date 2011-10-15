5d6257724b48cc31f5f20e84aa8be9a2812af07c
Nick Wiltsie <nwiltsie@mit.edu>
2011-10-15 13:12:16 -0400

    The "Look up a year" form on the Buy page works now.
    If the database lists the year as in stock, it forwards you to the
    buy page; otherwise you are sent to an apology page with the
    tnq-orders email if you want to inquire about rare copies.

--- purchase/urls.py
+++ purchase/urls.py
@@ -5,12 +5,12 @@ from django.core.urlresolvers import reverse
 
 urlpatterns = patterns('',
 
-    url(r'^$', direct_to_template, {'template' : 'tnq_site/buy.html'}, name='tnq_buy'),
+    url(r'^$', 'purchase.views.buy_info', name='tnq_buy'),
     (r'^order/', redirect_to, {'url' : '/buy/' } ), #For some reason using "reverse('tnq_buy')" doesn't work here!
     
     url(r'^book/(?P<book_year>\d{4})/', 'purchase.views.buy_form', {'purchase_option':'book'}, name='buy_book'),
     url(r'^seniorbundle/', 'purchase.views.buy_form', {'purchase_option':'senior_bundle'}, name='buy_senior_bundle'),
     url(r'^freshmanbundle/', 'purchase.views.buy_form', {'purchase_option':'freshman_bundle'}, name='buy_freshman_bundle'),
-    url(r'^patron/', 'purchase.views.buy_form', {'purchase_option':'patron'}, name='buy_patron'),
+    url(r'^patron/', 'purchase.views.patron_form', name='buy_patron'),
     
 )
--- purchase/views.py
+++ purchase/views.py
@@ -1,11 +1,33 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 from django.core.exceptions import ObjectDoesNotExist
 from mainsite.models import Setting
 from purchase.models import Book
+from django import forms
+
+class BookForm(forms.Form):
+    tnq_year = int(Setting.objects.get(tag="tnq_year").value)
+    year = forms.IntegerField(max_value=tnq_year, 
+                              min_value=1885, 
+                              required=False,
+                              error_messages={'max_value' : "We haven't made that book yet!",
+                                              'min_value' : "Technique didn't exist before 1885!",
+                                              'invalid' : "Enter a whole number between 1885 and %s" % tnq_year})
+    
+def buy_info(request):
+    if request.method == 'POST': #If the form has been submitted
+        form = BookForm(request.POST)
+        if form.is_valid() and form.cleaned_data['year']:
+            return HttpResponseRedirect(reverse('buy_book', args=[form.cleaned_data['year']]))
+
+    else:
+        form = BookForm()
+
+    return render_to_response('tnq_site/buy.html', {'form' : form})
+    
       
 def buy_form(request, purchase_option=None, book_year=None):
 
@@ -43,25 +65,7 @@ def buy_form(request, purchase_option=None, book_year=None):
         
         order_tag = "%s-Book" % book_year
         order_tag_pretty = "a copy of Technique %s" % book_year
-        
-    elif option=="patron":
-        try:
-            platinum_price = Setting.objects.get(tag="platinum_price").value
-            gold_price = Setting.objects.get(tag="gold_price").value
-            silver_price = Setting.objects.get(tag="silver_price").value
-            bronze_price = Setting.objects.get(tag="bronze_price").value
             
-            shipping_price = 0
-        except ObjectDoesNotExist:
-            pass
-        
-        return render_to_response('tnq_site/buy/patron_form.html', {'platinum_price' : platinum_price,
-                                                                     'gold_price' : gold_price,
-                                                                     'silver_price' : silver_price,
-                                                                     'bronze_price' : bronze_price,
-                                                                     'shipping_price' : 0,
-                                                                     'book_year' : book_year })
-    
     else:
         return HttpResponseRedirect(reverse('tnq_buy'))
         
@@ -69,3 +73,25 @@ def buy_form(request, purchase_option=None, book_year=None):
                                                                  'order_tag_pretty' : order_tag_pretty,
                                                                  'amount' : amount,
                                                                  'shipping_price' : shipping_price })
+                                                                 
+def patron_form(request):
+    try:
+        platinum_price = Setting.objects.get(tag="platinum_price").value
+        gold_price = Setting.objects.get(tag="gold_price").value
+        silver_price = Setting.objects.get(tag="silver_price").value
+        bronze_price = Setting.objects.get(tag="bronze_price").value
+    
+        shipping_price = 0
+        book_year = "2012"
+        
+    except ObjectDoesNotExist:
+        #Something needs to go here - at best the javascript will choke if there is no value,
+        #and at worst it might allow sales of $0.
+        pass
+
+    return render_to_response('tnq_site/buy/patron_form.html', {'platinum_price' : platinum_price,
+                                                                'gold_price' : gold_price,
+                                                                'silver_price' : silver_price,
+                                                                'bronze_price' : bronze_price,
+                                                                'shipping_price' : 0,
+                                                                'book_year' : book_year })
