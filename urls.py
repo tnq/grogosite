3a4de553d58f7656a017e4bf2f79d833a0246f7f
Nick Wiltsie <nwiltsie@mit.edu>
2011-11-11 12:17:44 -0500

    Added password reset functionality

--- urls.py
+++ urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls.defaults import *
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.simple import direct_to_template, redirect_to
-
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
-
-# Dajaxice import
-from dajaxice.core import dajaxice_autodiscover
+import django.contrib.auth.views
 from django.conf import settings
-dajaxice_autodiscover()
-
+from django.contrib import admin
 import massadmin.massadmin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
@@ -41,8 +36,16 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     url(r'^login/', 'django.contrib.auth.views.login', name='login-page'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout-page'),
-    
+
+    # Password reset
+    url(r'^password-reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
+    url(r'^password-reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
+    url(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
+    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
+
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Orders uploading page
     url(r'^admin/creditcard/upload/$', 'creditcard.views.upload', name='orders_upload'),
 )
 
