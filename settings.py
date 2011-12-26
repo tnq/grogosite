2bce402d67fc9715cdc4c9395757951731fa397a
Nick Wiltsie <nwiltsie@mit.edu>
2011-12-26 00:27:58 -0700

    Cleaned up settings.py, and switched to filesystem caching.
    The database caching does not play nice with the GIS databases, so I switched
    to using the filesystem cache.  The cache folder has to exist so it should
    probably be in a /media/cache or just /cache folder within the project.
    
    The wiki maps have Javascript errors and don't work yet.
    
    The wiki detail views ( /_history links and the like) are not password
    protected yet.  I'd like a better solution than to edit every view to have the
    @login_required method decorator, but I'll go with what works.

--- settings.py
+++ settings.py
@@ -1,14 +1,12 @@
-# Django settings for orders project.
+# Django settings for Technique project.
 
-import os, sys
-sys.path.append( os.path.join( os.path.dirname(os.path.abspath(__file__)), "localwiki" ) )
-sys.path.append( os.path.join( os.path.dirname(os.path.abspath(__file__)), "localwiki", "sapling" ) )
+import os.path, sys
 
 DEBUG = True 
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('yearbook', 'nwiltsie@mit.edu'),
+    ('Nick Wiltsie', 'nwiltsie@mit.edu'),
     ('Quentin Smith', 'quentin@mit.edu'),
 )
 
@@ -20,10 +18,17 @@ DATABASES = {
         'NAME': 'wiki.db',
         'USER': '',
         'PASSWORD': '',
-        'DATABASE_HOST': '',
-        'DATABASE_PORT': ''             # Set to empty string for default. Not used with sqlite3.
+        'HOST': '',
+        'PORT': ''             # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+# Path to root directory of django project
+DJANGO_ROOT = os.path.join( os.path.dirname(os.path.abspath(__file__)) )
+
+#Add wiki to path so apps can be imported without localwiki.sapling prefix
+sys.path.append( os.path.join(DJANGO_ROOT, "localwiki") )
+sys.path.append( os.path.join(DJANGO_ROOT, "localwiki", "sapling") )
 
 GLOBAL_LICENSE_NOTE = """<p>Except where otherwise noted, this content is licensed under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution License</a>. See <a href="/Copyrights">Copyrights.</p>"""
 
@@ -31,9 +36,8 @@ EDIT_LICENSE_NOTE = """<p>By clicking "Save Changes" you are agreeing to release
 
 SIGNUP_TOS = """I agree to release my contributions under the <a href="http://creativecommons.org/licenses/by/3.0/" target="_blank">Creative Commons-By license</a>, unless noted otherwise. See <a href="/Copyrights" target="_blank">Copyrights</a>."""
 
-#import os.path
-#if os.path.exists("settings_private.py"):
-#    execfile("settings_private.py")
+if os.path.exists("settings_private.py"):
+    execfile("settings_private.py")
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/admin/'
@@ -57,13 +61,13 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = 'media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 #MEDIA_URL = ''
-MEDIA_URL = "/static/junkmedia/"
+MEDIA_URL = "http://technique.mit.edu/media/"
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -87,24 +91,21 @@ AUTH_PROFILE_MODULE = 'checkout.User'
 AUTHENTICATION_BACKENDS = (
     'users.backends.CaseInsensitiveModelBackend',
     'users.backends.RestrictiveBackend',
-    #'django.contrib.auth.backends.RemoteUserBackend',
+    'django.contrib.auth.backends.RemoteUserBackend',
 )
 
-#CACHES = {
-#    'default': {
-#        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-#        'LOCATION': 'cache_table',
-#    }
-#}
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/tmp/django/',
+    }
+}
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '99&xeaxqq+om4i1lhqid#&olgq1crnah#9b)bz(j7kub(s84v9'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    #'django.template.loaders.filesystem.load_template_source',
-    #'django.template.loaders.app_directories.load_template_source',
-    #'django.template.loaders.eggs.load_template_source',
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 )
@@ -113,7 +114,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    #'django.middleware.csrf.CsrfResponseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.RemoteUserMiddleware',
 )
@@ -201,13 +201,11 @@ SHOW_IP_ADDRESSES = False
 
 LOGIN_REDIRECT_URL = '/'
 
-#HAYSTACK_SITECONF = 'sapling.search_sites'
-#HAYSTACK_SEARCH_ENGINE = 'solr'
-#HAYSTACK_SEARCH_ENGINE = 'dummy'
 HAYSTACK_CONNECTIONS = {
-'default': {
-'ENGINE':'',
-'URL' : ''},
+    'default': {
+        'ENGINE':'',
+        'URL' : ''
+    },
 }
 
 THUMBNAIL_BACKEND = 'utils.sorl_backends.AutoFormatBackend'
@@ -216,13 +214,6 @@ OL_API = STATIC_URL + 'openlayers/OpenLayers.js?tm=1317359250'
 OLWIDGET_CSS = '%solwidget/css/sapling.css?tm=1317359250' % STATIC_URL
 OLWIDGET_JS = '%solwidget/js/olwidget.js?tm=1317359250' % STATIC_URL
 CLOUDMADE_API = '%solwidget/js/sapling_cloudmade.js?tm=1317359250' % STATIC_URL
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS += (
-#    'django.template.loaders.filesystem.Loader',
-#    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.load_template_source',
-)
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     "utils.context_processors.sites.current_site",
@@ -239,37 +230,21 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 MIDDLEWARE_CLASSES += (
-    #'django.middleware.common.CommonMiddleware',
-    #'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
     'versionutils.versioning.middleware.AutoTrackUserInfoMiddleware',
     'redirects.middleware.RedirectFallbackMiddleware',
 )
 
-#TEMPLATE_DIRS += (
-#    os.path.join(DATA_ROOT, 'templates'),
-#    os.path.join(PROJECT_ROOT, 'templates'),
-#)
-
 INSTALLED_APPS += (
-    # Django-provided apps
-    #'django.contrib.auth',
-    #'django.contrib.admin',
     'django.contrib.gis',
-    #'django.contrib.contenttypes',
-    #'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sites',
-    #'django.contrib.staticfiles',
 
     # Other third-party apps
     'haystack',
     'olwidget',
     'registration',
     'sorl.thumbnail',
-    #'staticfiles',
     'guardian',
     'south',
 
@@ -286,55 +261,24 @@ INSTALLED_APPS += (
     'utils',
 )
 
-LOCAL_INSTALLED_APPS = ()
-# We set this manually, after localsettings import, below.
-#TEMPLATE_DIRS = ()
-
 SITE_THEME = 'sapling'
-
-# Where localsettings.py lives
-#sys.path.append(os.path.join(DATA_ROOT, 'conf'))
-#try:
-#    from localsettings import *
-#except:
-#    pass
-
-# Allow localsettings.py to define LOCAL_INSTALLED_APPS.
-#INSTALLED_APPS = tuple(list(INSTALLED_APPS) + list(LOCAL_INSTALLED_APPS))
 
 ###############################
 # Setup template directories
 ###############################
-#LOCAL_TEMPLATE_DIR = os.path.join(DATA_ROOT, 'templates')
-#PROJECT_TEMPLATE_DIR = os.path.join(PROJECT_ROOT, 'templates')
+PROJECT_TEMPLATE_DIR = os.path.join(PROJECT_ROOT, 'templates')
 
-# A site theme uses a template directory with a particular name.
-# Site themes can live in either the global themes/ directory
-# or in the local themes/ directory (in DATA_ROOT).
-#PROJECT_THEME_TEMPLATE_DIR = os.path.join(PROJECT_ROOT, 'themes', SITE_THEME, 'templates')
-#LOCAL_THEME_TEMPLATE_DIR = os.path.join(DATA_ROOT, 'themes', SITE_THEME, 'templates')
+PROJECT_THEME_TEMPLATE_DIR = os.path.join(PROJECT_ROOT, 'themes', SITE_THEME, 'templates')
 
-#TEMPLATE_DIRS = tuple([LOCAL_TEMPLATE_DIR, PROJECT_TEMPLATE_DIR, LOCAL_THEME_TEMPLATE_DIR, PROJECT_THEME_TEMPLATE_DIR] +
-#                      list(TEMPLATE_DIRS))
 TEMPLATE_DIRS += (
-    'localwiki/sapling/templates',
-    'localwiki/sapling/themes/sapling/templates',
-    
+    PROJECT_TEMPLATE_DIR,
+    PROJECT_THEME_TEMPLATE_DIR,
 )
 
+_global_theme_dir = os.path.join(PROJECT_ROOT, 'themes', SITE_THEME, 'assets')
 
-#STATICFILES_DIRS = []
-# A site theme uses a static assets directory with a particular name.
-# Site themes can live in either the global themes/ directory
-# or in the local themes/ directory (in DATA_ROOT).
-#_local_theme_dir = os.path.join(DATA_ROOT, 'themes', SITE_THEME, 'assets')
-#_global_theme_dir = os.path.join(PROJECT_ROOT, 'themes', SITE_THEME, 'assets')
-#if os.path.exists(_local_theme_dir):
-#    STATICFILES_DIRS.append(('theme', _local_theme_dir))
-#if os.path.exists(_global_theme_dir):
-#    STATICFILES_DIRS.append(('theme', _global_theme_dir))
+if os.path.exists(_global_theme_dir):
+    STATICFILES_DIRS += (('theme', _global_theme_dir),)
 
-STATICFILES_DIRS += (('theme', os.path.join(PROJECT_ROOT, 'themes', SITE_THEME, 'assets')),)
-
-STATIC_ROOT = "junkstatic"
+STATIC_ROOT = os.path.join(DJANGO_ROOT, "static")
 
