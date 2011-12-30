215e26d5001c53409154de47fd86809304ab3591
Nick Wiltsie <nwiltsie@mit.edu>
2011-12-30 12:59:30 -0700

    Cleaned up site_settings.py.
    Removed old do_evaluate tag (used when we had FlatPages) and corrected the
    in_group tag to actually do what the documentation said it should.

--- mainsite/templatetags/site_settings.py
+++ mainsite/templatetags/site_settings.py
@@ -13,35 +13,12 @@ def tnq_setting(tag):
         value = Setting.objects.get(tag=tag).value
     except ObjectDoesNotExist:
         if settings.DEBUG:
-            value = "TAG %s NOT FOUND!" %(tag)
+            value = "TAG %s NOT FOUND!" % (tag)
         else:
             value = ""
     return value
 
 register.simple_tag(tnq_setting)    
-
-@register.tag(name="evaluate")
-def do_evaluate(parser, token):
-    """
-    tag usage {% evaluate object.textfield %}
-    """
-    try:
-        tag_name, variable = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError, "%r tag requires a single argument" % token.contents.split()[0]
-    return EvaluateNode(variable)
-
-class EvaluateNode(template.Node):
-    def __init__(self, variable):
-        self.variable = template.Variable(variable)
-
-    def render(self, context):
-        try:
-            content = self.variable.resolve(context)
-            t = template.Template(content)
-            return t.render(context)
-        except template.VariableDoesNotExist, template.TemplateSyntaxError:
-            return 'Error rendering', self.variable
 
 @register.filter
 def sample(value, arg):
@@ -49,28 +26,28 @@ def sample(value, arg):
 
 @register.filter
 def in_group(user, group):
-	"""Return True/False if the user is in the given group(s).
-	Usage::
-		{% if user|in_group:"Friends" %}
-		or
-		{% if user|in_group:"Friends,Enemies" %}
-		...
-		{% endif %}
-	You can specify a single group or comma-delimited list.
-	No white space allowed.
-	"""
+    """Return true if the user is in any of the the given groups.
+    Usage::
+        {% if user|in_group:"Friends" %}
+        or
+        {% if user|in_group:"Friends,Enemies" %}
+        ...
+        {% endif %}
+    You can specify a single group or comma-delimited list.
+    No white space allowed.
+    """
 
-	if re.search(',', group):
-	    group_list = re.sub('\s+','',group).split(',')
-	elif re.search(' ', group):
-	    group_list = group.split()
-	else:
-	    group_list = [group]
+    if re.search(',', group):
+        group_list = re.sub('\s+','',group).split(',')
+    elif re.search(' ', group):
+        group_list = group.split()
+    else:
+        group_list = [group]
 
-	user_groups = []
-	for group in user.groups.all():
-	    user_groups.append(str(group.name))
+    user_groups = []
+    for group in user.groups.all():
+        user_groups.append(str(group.name))
 
-	return filter(lambda x:x in user_groups, group_list)
+    return len([group for group in group_list if group in user_groups]) > 0
 
 in_group.is_safe = True
