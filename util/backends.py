da0e6e99e6123a0b231de9eca4e52168a5a247d1
Quentin Smith <quentin@mit.edu>
2012-02-03 23:22:39 -0500

    Kerberized e-mail backend

--- util/backends.py
+++ util/backends.py
@@ -1,4 +1,86 @@
 from django.contrib.auth.backends import RemoteUserBackend
 
+import threading
+import tempfile
+
+from django.conf import settings
+from django.core.mail.backends.base import BaseEmailBackend
+from subprocess import Popen,PIPE,check_call
+
 class TNQRemoteUserBackend(RemoteUserBackend):
     create_unknown_user = False
+
+class SendmailBackend(BaseEmailBackend):
+    """sendmail email backend class."""
+    def __init__(self, fail_silently=False, sendmail_cmd=None, **kwargs):
+        super(SendmailBackend, self).__init__(fail_silently=fail_silently)
+        self.sendmail_cmd = sendmail_cmd
+        if self.sendmail_cmd is None:
+            try:
+                self.sendmail_cmd = settings.SENDMAIL_CMD
+            except:
+                self.sendmail_cmd = "sendmail"
+        if isinstance(self.sendmail_cmd, str):
+            mailer = self.sendmail_cmd.split()
+        self._lock = threading.RLock()
+
+    def open(self):
+        return True
+
+    def close(self):
+        pass
+
+    def send_messages(self, email_messages):
+        """
+        Sends one or more EmailMessage objects and returns the number of email
+        messages sent.
+        """
+        if not email_messages:
+            return
+        self._lock.acquire()
+        try:
+            num_sent = 0
+            for message in email_messages:
+                sent = self._send(message)
+                if sent:
+                    num_sent += 1
+        finally:
+            self._lock.release()
+        return num_sent
+
+    def _get_environ(self):
+        return os.environ
+
+    def _send(self, email_message):
+        """A helper method that does the actual sending."""
+        if not email_message.recipients():
+            return False
+        try:
+            ps = Popen(self.sendmail_cmd+list(email_message.recipients()), \
+                       stdin=PIPE, env=self._get_environ())
+            ps.stdin.write(email_message.message().as_string())
+            ps.stdin.flush()
+            ps.stdin.close()
+            return not ps.wait()
+        except:
+            if not self.fail_silently:
+                raise
+            return False
+        return True
+
+class KerberizedSendmailBackend(SendmailBackend):
+    def __init__(self, fail_silently=False, keytab_path, principal, **kwargs):
+        super(KerberizedSendmailBackend, self).__init__(fail_silently=fail_silently, **kwargs)
+        self.keytab_path = keytab_path
+        self.principal = principal
+        _, self.ccache_path = tempfile.mkstemp(prefix="krb5")
+
+    def _get_environ(self):
+        env = os.environ
+        env["KRB5CCNAME"] = self.ccache_path
+        return env
+
+    def open(self):
+        super(KerberizedSendmailBackend, self).open()
+        check_call(["kinit", "-k", "-t", self.keytab_path, self.principal], env=self._get_environ())
+        return True
