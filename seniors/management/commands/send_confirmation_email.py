6a660eea0c1ddc0663968ffc84b923b1b23bfa80
Nick Wiltsie <nwiltsie@mit.edu>
2012-02-04 22:29:34 -0500

    Changed senior emailer to only use one mail connection

--- seniors/management/commands/send_confirmation_email.py
+++ seniors/management/commands/send_confirmation_email.py
@@ -7,7 +7,7 @@ import subprocess
 
 from django.conf import settings
 from django.core.management.base import LabelCommand, CommandError
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, get_connection
 
 from mainsite.models import Setting
 from seniors.models import Senior
@@ -17,18 +17,18 @@ class Command(LabelCommand):
     help = "Send email confirmations to all the current year's seniors in the database."
 
     def handle_label(self, orig_dir, **options):
-    
+
         tnq_year = Setting.objects.get(tag="tnq_year").value
         tnq_year = 2013
-    
+
         if raw_input("This is going to send a LOT of emails to the %s seniors. Are you sure? [yN] " % tnq_year).lower() != "y":
             return
         if raw_input("Super sure? [yN] ").lower() != "y":
             return
-            
+
         convert_command = "convert %s -resize x200 -fill 'rgba(255,0,255,0.5)' -gravity center -pointsize 60 -font /Users/nwiltsie/Library/Fonts/AvenirLTStd-Light.otf  -annotate 330 'TNQ' jpg:-"
         small_photo_dir = settings.SENIOR_IMAGE_DIRECTORY
-    
+
         base_message = """Hi Senior!
 
 Your friendly Technique staphers are busily laying out the Senior section of the yearbook, and we want to do one final check of your picture and data before we submit them.
@@ -53,11 +53,15 @@ Home Town: %s
 Home State (or Country): %s
 Living Group: %s
 """
+
+        connection = get_connection()
+        connection.open()
+
         seniors = Senior.objects.filter(tnq_year=tnq_year).exclude(image_path=None)
         for senior in seniors:
-        
+
             file_name = "%s-%s.jpg" % (senior.id, senior.kerberos)
-        
+
             message = base_message % (tnq_year,
                              senior.name,
                              file_name,
@@ -71,7 +75,7 @@ Living Group: %s
                 message += "\nQuote: %s" % senior.quote
             if senior.quote_author:
                 message += "\nQuote Author: %s" % senior.quote_author
-                
+
             message += "\n"
 
             for activity in senior.activity_set.all():
@@ -86,8 +90,8 @@ Living Group: %s
             recipient = ["%s@mit.edu" % senior.kerberos]
             bcc = ["tnq-seniors-info@mit.edu"]
             sender = "tnq-seniors@mit.edu"
-            email_message = EmailMessage(subject, message, sender, recipient, bcc)
-            
+            email_message = EmailMessage(subject, message, sender, recipient, bcc, connection=connection)
+
             convert_args = shlex.split(convert_command % str(os.path.join(orig_dir, senior.image_path)) )
             image = subprocess.check_output(convert_args)
             image_file = open(os.path.join(small_photo_dir,file_name), "wb")
@@ -95,6 +99,8 @@ Living Group: %s
             image_file.close()
 
             email_message.attach(file_name, image, 'image/jpeg')
-            
             email_message.send()
-            
+
+            print "Sent message to %s" % senior.name
+
+        connection.close()
