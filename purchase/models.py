9d00bdbc3bdafc121fb714507565906c77112969
akimitsu <akimitsu@mit.edu>
2013-05-18 00:53:22 -0400

    Added inventory fields to book model

--- purchase/models.py
+++ purchase/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     year = models.IntegerField(unique=True)
-    price = models.IntegerField()
-    
+    original_inventory = models.IntegerField()
+    current_inventory = models.IntegerField(editable=False)
+    price = models.IntegerField(editable=False)
+                
     def __unicode__(self):
-	    return "%d ($%d)" % (self.year, self.price)
+	    return "%d (%d left, $%d)" % (self.year, self.current_number, self.price)
 
     class Meta:
         ordering = ['-year']
