fa671a140e6843fe322e1156e783f65c89d801f7
Quentin Smith <quentin@mit.edu>
2015-03-18 11:41:37 -0400

    Allow specifying the year of a senior bundle

--- purchase/urls.py
+++ purchase/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     (r'^order/', RedirectView.as_view(url='/buy/')), #For some reason using "reverse('tnq_buy')" doesn't work here!
     
     url(r'^book/(?P<book_year>\d{4})/', 'purchase.views.buy_form', {'purchase_option':'book'}, name='buy_book'),
-    url(r'^seniorbundle/', 'purchase.views.buy_form', {'purchase_option':'senior_bundle'}, name='buy_senior_bundle'),
+    url(r'^seniorbundle/(?P<book_year>\d{4})?', 'purchase.views.buy_form', {'purchase_option':'senior_bundle'}, name='buy_senior_bundle'),
     url(r'^freshmanbundle/', 'purchase.views.buy_form', {'purchase_option':'freshman_bundle'}, name='buy_freshman_bundle'),
     url(r'^patron/', 'purchase.views.patron_form', name='buy_patron'),
     
